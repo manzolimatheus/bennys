c47bf5e18deb7b73d4e701ac0461b21cd27a32a0
Matheus Manzoli <manzoli.contato@gmail.com>
2023-10-25 01:43:12 +0000

    feat: add animal model

---
+++ care/src/main/java/com/bennys/care/models/Animal.java
@@ -0,0 +1,36 @@
+package com.bennys.care.models;
+
+public class Animal {
+    private String nome;
+    private String especie;
+    private String dono;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+}
